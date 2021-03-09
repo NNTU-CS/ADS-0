aeda2b3e1d251b8ad9b5c217521647c556bc14c0
GurenkoNick <78843007+GurenkoNick@users.noreply.github.com>
2021-03-09 11:13:49 +0300

    Гуренко Никита

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,7 +3,7 @@
 int gcd(int a, int b) {
     // поместить сюда текст реализации алгоритма (на С++)
     if (b == 0)
-		return a;
-	else
-		return gcd (b, a % b);
+         return a;
+    else
+         return gcd (b, a % b);
 }

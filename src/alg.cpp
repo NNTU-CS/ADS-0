11d729c357b51c69afcca11ac1ccf185ebb92d28
jollynavechno <77914359+jollynavechno@users.noreply.github.com>
2021-03-09 22:46:54 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,7 +1,5 @@
-// Copyright 2021 NNTU-CS
-
-int gcd(int a, int b) {
-    if (a % b == 0 || b % a == 0) {
+int gcd(int a, int b){
+	if (a % b == 0 || b % a == 0) {
 		if (a > b) return b;
 		else return a;
 	}

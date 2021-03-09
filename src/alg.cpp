3b9e57848d616c75eea5f01f1cac2fd043d4d8b9
KrasilnikovAA <79089177+KrasilnikovAA@users.noreply.github.com>
2021-03-09 11:22:12 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,10 +2,12 @@
 
 int gcd(int a, int b) {
     // поместить сюда текст реализации алгоритма (на С++)
- while (a!=0 and b!=0) {
-     if (a>b)
-     a = a % b
-         else
-     b = b % a;
+ while (a!=b) {
+     if (a > b) {
+     a = a - b
+     } else {
+     b = b - a;
  }
+ }
+    return a;
 }

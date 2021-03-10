f0b777fa9e06440e3b6c045ec086bea924215686
TedZumi <79110264+TedZumi@users.noreply.github.com>
2021-03-10 21:12:52 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,9 +1,8 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-        while ((a!=0) and (b!=0))
-        {
-                if (a>b)
+        while ((a != 0) && (b != 0)) {
+                if (a > b)
                         a%=b;
                 else
                         b%=a;

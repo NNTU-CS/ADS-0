366d3da3583e58d83e07fa88987c64276aecd4fb
younesadjoudj <79196405+younesadjoudj@users.noreply.github.com>
2021-06-06 02:12:40 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,12 +1,8 @@
 // Copyright 2021 NNTU-CS
-
 int gcd(int a, int b) {
-    // поместить сюда текст реализации алгоритма (на С++)
-  int c;
-  while (b != 0) {
-    c = b;
-    b = a % b;
-    a = c;
-  }
-  return a;
+      if (b == 0)
+    return a;
+  return gcd(b, a % b);
 }
+   
+  

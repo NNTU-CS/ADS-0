24b0647867f4860f676f98d225180374a6d9e44a
Илья Прокошин <117671181+IliaProkoshin@users.noreply.github.com>
2024-02-21 14:30:12 +0300

    Update alg.cpp
    Codestyle

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,13 +4,13 @@ int gcd(int a, int b) {
     int max = a;
     int min = b;
     int boofer;
-    if (b > max){
+    if (b > max) {
         max = b;
         min = a;
     }
-    while (max and min){
+    while (max && min) {
         max = max % min;
-        if (min > max){
+        if (min > max) {
             boofer = max;
             max = min;
             min = boofer;

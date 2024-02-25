72c1e0927739d91f3239cac9a4ccbd67faf6febd
shorokhovkir <160240169+shorokhovkir@users.noreply.github.com>
2024-02-25 19:29:08 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,12 +1,14 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    // поместить сюда текст реализации алгоритма (на С++)
-    while (a != b) {
-        if (a > b)
-	    a = a - b;
-	else
-	    b = b - a;
-    }
-    return a;
+	while (a != 0 && b != 0) {
+		if (a > b)
+			a = a - b;
+		else
+			b = b - a;
+	}
+	if (a != 0)
+		return a;
+	else 
+		return b;
 }

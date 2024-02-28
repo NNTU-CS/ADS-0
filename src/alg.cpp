b4354f4724c789e23beee2b1aab8cb39d7a63052
shorokhovkir <160240169+shorokhovkir@users.noreply.github.com>
2024-02-28 17:53:35 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,16 +1,14 @@
 // Copyright 2021 NNTU-CS
 
-int gcd(int a, int b) 
-{
-    while (a != 0 && b != 0) 
-    {
-        if (a > b)
-            a = a - b;
-        else
-            b = b - a;
-    }
-    if (a != 0)
-        return a;
-    else 
-        return b;
+int gcd(int a, int b) {
+  while (a != 0 && b != 0) {
+    if (a > b)
+      a = a - b;
+    else
+      b = b - a;
+  }
+  if (a != 0)
+    return a;
+  else 
+    return b;
 }

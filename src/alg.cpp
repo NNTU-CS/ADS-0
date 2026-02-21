7a2eb7395ef6ed1327ed7a3dcf91803e810bab8e
ulyanab1337-sys <ulyanab1337@gmail.com>
2026-02-21 17:21:24 +0300

    task 0 Babushkina 25-ist-2

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,15 +2,15 @@
 #include <iostream>
 #include "alg.h"
 
-
 int gcd(int a, int b) {
-	while(b != 0) {
-		int temp = b;
-		b = a % b;
-		a = temp;
-	}
-	return abs(a);
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return abs(a);
 }
+
 int main() {
     std::cout << gcd(10, 15) << std::endl;
     return 0;

5ad01dc7b78fbbf39533b104c22584dee688bf8a
errfct <veronika.sokolova.07@mail.ru>
2026-02-23 17:00:03 +0300

    Code-1-1 alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,11 @@
-// Copyright 2021 NNTU-CS
-
 int gcd(int a, int b) {
-	return 25;
+    a = std::abs(a);
+    b = std::abs(b);
+
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
 }

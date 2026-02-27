be9d0a6c2cbf71a329da347e5a278b748f657697
Dodoroshka <yanovaanastasia18@gmail.com>
2026-02-27 21:44:00 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,12 +2,12 @@
 
 int gcd(int a, int b) {
 	while (a !=0 and b !=0 ){
-    	if (a > b) {
-        	a = a % b;
-    	}
-    	else {
-        	b = b % a;  
-    	}
+		if (a > b) {
+			a = a % b;
+		}
+		else {
+			b = b % a;
+		}
 	}
 	return a+b;
 }

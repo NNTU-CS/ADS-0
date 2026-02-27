30ff14b24ec4008cea60dfcd19e1123d93f03b20
Dodoroshka <yanovaanastasia18@gmail.com>
2026-02-27 22:28:56 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,13 +1,12 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-	while (a !=0 and b !=0 ){
-		if (a > b) {
-			a = a % b;
-		}
-		else {
-			b = b % a;
-		}
-	}
-	return a+b;
+    while (a != 0 && b != 0) {
+        if (a > b) {
+            a = a % b;
+        } else {
+            b = b % a;
+        }
+    }
+    return a + b;
 }

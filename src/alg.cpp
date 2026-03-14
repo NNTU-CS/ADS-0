22756e08025d094b0fc0dbbd3cfa05eb6da3845c
krotolik2020-ctrl <Kro.tolik2020@gmail.com>
2026-03-14 16:00:04 +0300

    add prime and gcd

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,23 +1,8 @@
 // Copyright 2021 NNTU-CS
+// Copyright 2021 NNTU-CS
+#include "alg.h"
 
-#include <algorithm>
-#include <cstdint>
-
-bool isPrime(uint64_t n) {
-  if (n < 2) return false;
-  for (uint64_t i = 2; i * i <= n; i++) {
-    if (n % i == 0) return false;
-  }
-  return true;
-}
-
-uint64_t nextPrime(uint64_t n) {
-  uint64_t next = n + 1;
-  while (!isPrime(next)) {
-    next++;
-  }
-  return next;
-}
+#include <utility>
 
 int gcd(int a, int b) {
   while (b) {
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 // Copyright 2021 NNTU-CS
 #include <iostream>
+
 #include "alg.h"
 
-int main() {
-    int result = gcd(10, 15);
-    std::cout << result << std::endl;
-    return 0;
+int gcd(int a, int b) {
+  while (b) {
+    a %= b;
+    std::swap(a, b);
+  }
+  return a;
 }
